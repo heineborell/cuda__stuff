7478bbdb57aadbc1c0e180b025ab6c1c094ea73f
Deniz <dodeve@gmail.com>
2026-03-23 00:25:42 +0900

    started ode solver

---
+++ ode_solver/trapeozoid.cpp
@@ -0,0 +1,66 @@
+#include "helper.h"
+#include <cmath>
+#include <cstddef>
+#include <iomanip>
+#include <iostream>
+#include <numbers>
+#include <vector>
+
+double leftRectangleArea(std::vector<double> &xk, double dx, int N) {
+  double area{0.0f};
+  for (int i{0}; i < N; ++i) {
+    area = area + sin(xk.data()[i]) * dx;
+  }
+  return area;
+}
+
+double rightRectangleArea(std::vector<double> &xk, double dx, int N) {
+  double area{0.0f};
+  for (int i{1}; i <= N; ++i) {
+    area = area + sin(xk.data()[i]) * dx;
+  }
+  return area;
+}
+
+double trapezoidArea(std::vector<double> &xk, double dx, int N) {
+  double area{0.0f};
+  for (int i{0}; i < N; ++i) {
+    area = area + dx / 2 * (sin(xk.data()[i]) + sin(xk.data()[i + 1]));
+  }
+  return area;
+}
+
+void printArray(std::vector<double> &arr) {
+  for (double &item : arr) {
+    std::cout << item << '\n';
+  }
+}
+
+std::vector<double> vectorScale(std::vector<double> &arr, double scalar) {
+  std::vector<double> scaled;
+  scaled.reserve(size(arr));
+  for (double &item : arr)
+    scaled.push_back(scalar * item);
+  return scaled;
+}
+
+int main() {
+  Timer t;
+  double w{2 * std::numbers::pi};                     // natural frequency
+  double zeta{0.25};                                  // damping ratio
+  std::vector<double> A{0, 1, -w * w, -2 * zeta * w}; // A vector
+  std::vector<double> I{1, 0, 0, 1};                  // identity
+  double dt{0.01};                                    // time step
+  double T{10.0};                                     // total time
+
+  std::vector<double> x0{2, 0}; // initial condition
+  int N{static_cast<int>(T / dt)};
+
+  std::cout << "number of time intervals " << N << '\n';
+  std::vector<double> scaledVec{vectorScale(A, dt)};
+  printArray(A);
+  printArray(scaledVec);
+
+  std::cout << "total time is " << t.elapsed() << '\n';
+  return 0;
+}
