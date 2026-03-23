775c885b0b351dbd2e2787c64044248b1e06b1dd
deniz deveci <dodeve@gmail.com>
2026-03-23 13:50:49 +0900

    working mm

--- ode_solver/solver.cpp
+++ ode_solver/solver.cpp
@@ -44,10 +44,27 @@ std::vector<double> vectorScale(std::vector<double> &arr, double scalar) {
   return scaled;
 }
 
+// matrix multiply AB=C
+void mm(std::vector<double> &A, std::vector<double> &B, std::vector<double> &C,
+        int Nay, int Nbx) {
+  for (int y{0}; y < Nay; ++y) {
+    for (int x{0}; x < Nbx; ++x) {
+      double sum{0.0f};
+      for (int i{0}; i < Nay; ++i) {
+        sum += A.data()[y * Nay + i] * B.data()[i * Nay + x];
+      }
+      C.data()[y * Nay + x] = sum;
+    }
+  }
+}
+
 int main() {
   Timer t;
-  double w{2 * std::numbers::pi};                     // natural frequency
-  double zeta{0.25};                                  // damping ratio
+  double w{2 * std::numbers::pi}; // natural frequency
+  double zeta{0.25};              // damping ratio
+
+  int Nx{2};
+  int Ny{2};
   std::vector<double> A{0, 1, -w * w, -2 * zeta * w}; // A vector
   std::vector<double> I{1, 0, 0, 1};                  // identity
   double dt{0.01};                                    // time step
@@ -56,10 +73,16 @@ int main() {
   std::vector<double> x0{2, 0}; // initial condition
   int N{static_cast<int>(T / dt)};
 
-  std::cout << "number of time intervals " << N << '\n';
+  // std::cout << "number of time intervals " << N << '\n';
   std::vector<double> scaledVec{vectorScale(A, dt)};
-  printArray(A);
-  printArray(scaledVec);
+  std::vector<double> C(2, 0);
+  mm_og(I, x0, C, 2, 1);
+
+  // mm_cpu(I, x0, C, 2, 2, 0);
+
+  printArray(C);
+  // printArray(A);
+  // printArray(scaledVec);
 
   std::cout << "total time is " << t.elapsed() << '\n';
   return 0;
