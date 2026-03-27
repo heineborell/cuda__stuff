816e768a79c99481e02115d4c8c8f731548a21e1
deniz deveci <dodeve@gmail.com>
2026-03-27 17:51:03 +0900

    non crushing

--- ode_solver/rungeKuttaplot.cpp
+++ ode_solver/rungeKuttaplot.cpp
@@ -3,6 +3,7 @@
 #include <cstddef>
 #include <iostream>
 #include <vector>
+#include <iomanip>
 
 void printArray(std::vector<double> const &arr) {
   for (double const &item : arr) {
@@ -57,39 +58,64 @@ std::vector<double> vecAddCpu(std::vector<double> const &x,
 }
 
 
-std::vector<double> timeVec(int N, double dt){
-       std::vector<double> Tk(N,0.0);
-       for(int i{0}; i< N;++i)
-        Tk.data()[i+1]=Tk.data()[i]+dt;
-       return Tk;}
+void timeVec(std::vector<double>&vec,int gridDimX,int gridDimY, double dt){
+  int N {static_cast<int>(vec.size())};
+       for(int i{gridDimX*(gridDimY-1)}; i< N;++i)
+        vec.data()[i+1]=vec.data()[i]+dt;}  
 
-
-template <typename Func>
-void rk2firstOrder(std::vector<double> &X,std::vector<double> &T,const Func &fn, double dt, int N) {
-  for (int k{0}; k < N-1; ++k) {
-    X.data()[k + 1] = X.data()[k] +dt*fn(X.data()[k]+dt/2*fn(X.data()[k],T.data()[k]),T.data()[k]+dt/2) ;
+void showMatrix(std::vector<double> &vec,int gridDimX, int gridDimY) {
+  int N {static_cast<int>(vec.size())};
+  std::cout << std::setprecision(3);
+  for(int i {0}; i<N;++i){
+    if(i%gridDimX==0)
+      std::cout << '\n';
+    std::cout << vec[i] << " ";}
   }
-}
+
+
+
+
+void rk2firstOrder(std::vector<double> &X,double dt, int dimX, int dimY) {
+  //functions  to be integrated (rhs)
+  double sigma {10};
+  double rho {8.0/3.0};
+  double beta {28};
+
+  auto fnX=[sigma](double x,double y,double z ){return sigma*(y-x);};
+  auto fnY=[rho](double x,double y,double z ){return x*(rho-z)-y;};
+  auto fnZ=[beta](double x,double y,double z ){return x*y-beta*z;};
+
+    for(int x{0};x < dimX-1;++x)
+      for(int y{0};y < dimY-1;++y)
+      X.data()[y*dimX+(x+1)]=X.data()[y*dimX+x]+dt*fnX(X.data()[y*dimX+x],X.data()[(y+1)*dimX+x],X.data()[(y+2)*dimX+x]);
+ }
 
 int main() {
   // solver piece
 
   double dt{0.001};                                   
-  float xRange{2.5f};
+  float xRange{4.0f};
 
-  int N{static_cast<int>(xRange / dt)};
-  // create X and set initial value
-  std::vector<double> X(static_cast<std::size_t>(N), 0.0);
-  X.data()[0]= 1.0;
+  const int dimX{static_cast<int>(xRange / dt)};
+  // const int dimX{5};
+  constexpr int dimY {4};
+  
+  // create X,Y,Z and set initial value
+
+  std::vector<double> X(static_cast<std::size_t>(dimX*dimY), 0.0);
+  X.data()[0]= -8.0; //x initial
+  X.data()[dimX]= 8.0; // y initial
+  X.data()[2*dimX]= 27; // z initial 
 
   // time vector tk
-  std::vector<double> tk {timeVec(N, dt)};
-  //function to be integrated
-  auto rhs=[](double x,double t){return x*x;};
+  timeVec(X,dimX,dimY, dt);
 
   //integrator
-  rk2firstOrder(X,tk, rhs, dt, N);
+  showMatrix(X, dimX, dimY);
+  std::cout << "the processed matrix" << '\n';
+  rk2firstOrder(X, dt, dimX,dimY);
   std::vector<float> resultFloat{castArray(X)};
+  showMatrix(X, dimX, dimY);
 
   const int screenWidth{1960};
   const int screenHeight{800};
@@ -107,7 +133,7 @@ int main() {
     if (xRange > 50.0f)
       xRange = 50.0f;
 
-    float step{xRange / N}; // step size for plotting
+    float step{xRange / dimX}; // step size for plotting
 
     BeginDrawing();
     ClearBackground(BLACK);
@@ -117,7 +143,7 @@ int main() {
 
     DrawText("Y", screenWidth / 2 + 5, 5, 20, GRAY);
     DrawText("T", screenWidth - 20, screenHeight / 2 + 5, 20, GRAY);
-    for (std::size_t i{0}; i < N; ++i) {
+    for (std::size_t i{0}; i < dimX; ++i) {
       float x1{0 + i * step}; // current x value
       float x2{x1 + step};    // next x value
       float y1{resultFloat.data()[i]};
